c3856a7d8ef510382dc24f59fab2d1fcad1e9d69
Shermin Sultana <rima64549@gmail.com>
2023-08-26 15:09:13 +0600

    add

--- map.js
+++ map.js
@@ -1,9 +1,20 @@
 const numbers = [10, 11, 23, 32, 44, 22, 16, 65, 23];
+
 let sum = 0;
 const mapresult = numbers.map( x => {
     sum = sum + x ;
 })
 console.log(sum);
 
+// 2::
+const nums = [2, 2, 3]
+
+let multiply = 1;
+const result2 = nums.map( x => {
+    multiply = multiply * x ;
+})
+console.log( multiply);
+
+// 3:::
 const result = numbers.map ( x => x*5);
 console.log(result);
