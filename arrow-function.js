c70a13f73182778aa9e4c639702bf69bcfdccbf3
Shermin Sultana <rima64549@gmail.com>
2023-08-26 16:03:57 +0600

    add

---
+++ arrow-function.js
@@ -0,0 +1,27 @@
+// normal 1:: with parameter , with return
+
+function add(x, y){
+    return x + y;
+}
+ const result = add(5,5);
+ console.log(result)
+
+ // arrow::
+
+ const addarr = (x, y) =>{
+    return  x + y;
+ }
+ const arrResult = add(10,20);
+ console.log(arrResult);
+
+ // no parameter no return
+
+ function show(){
+    console.log("hello world");
+ }
+ show();
+// arrow
+ const arrShow = () => console.log("hello jerin");
+ arrShow();
+
+ 
